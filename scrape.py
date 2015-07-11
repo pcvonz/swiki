51e929bb387dc83533190e2a6f73f18a18fdd934
Paul Von Zimmerman <pcvonz@gmail.com>
2015-07-11 16:12:22 -0700

    Added language: spanish

--- scrape.py
+++ scrape.py
@@ -9,7 +9,7 @@ from flask import render_template
 #Grabs a random wikipedia article
 app = Flask(__name__)
 
-@app.route('/grab_article')
+@app.route('/grab_article_en')
 def grab_random_article():
     wiki_page = BeautifulSoup(urllib2.urlopen('http://en.wikipedia.org/wiki/Special:Random').read())
     wiki_page = wiki_page.findAll(id='mw-content-text')
@@ -20,11 +20,9 @@ def grab_random_article():
     #regular expression to get rid of all html tags
     tags = re.findall(r'<[^>]+>', wiki_page)
     
-    #regular expression to remove all citations
+    #regular expression to remove all citations (doesn't work yet...)
     brackets = re.findall(r'\[.\]', wiki_page)
-    print(brackets)
     returns = re.findall(r'\n', wiki_page)
-    
     tabs = re.findall(r'\t', wiki_page)
     
     for i in tags:
@@ -33,11 +31,48 @@ def grab_random_article():
         wiki_page = wiki_page.replace(i, '')
     for i in returns:
         wiki_page = wiki_page.replace(i, ' ')
-    #removing multiple spaces
 
-                        
+    #Removing space characters from wiki_page list
     wiki_page = wiki_page.split(" ")
+    wiki_page_new = []
+    for i in range(0, len(wiki_page)):
+        if wiki_page[i] != "":
+            wiki_page_new.append(wiki_page[i])
+            
+    wiki_json = json.JSONEncoder(ensure_ascii = False).encode(wiki_page_new)
     
+    #f = codecs.open('wiki_page', 'w', "utf-8")
+    #f.write(wiki_json)
+    #f.close()
+    
+    
+    return wiki_json
+
+@app.route('/grab_article_es')
+def grab_random_article_es():
+    wiki_page = BeautifulSoup(urllib2.urlopen('https://es.wikipedia.org/wiki/Especial:Aleatoria').read())
+    wiki_page = wiki_page.findAll(id='mw-content-text')
+    
+    #converting bs4 result set to unicode
+    wiki_page = unicode.join(u'\n',map(unicode, wiki_page))
+    
+    #regular expression to get rid of all html tags
+    tags = re.findall(r'<[^>]+>', wiki_page)
+    
+    #regular expression to remove all citations (doesn't work yet...)
+    brackets = re.findall(r'\[.\]', wiki_page)
+    returns = re.findall(r'\n', wiki_page)
+    tabs = re.findall(r'\t', wiki_page)
+    
+    for i in tags:
+        wiki_page = wiki_page.replace(i, '')    
+    for i in brackets:
+        wiki_page = wiki_page.replace(i, '')
+    for i in returns:
+        wiki_page = wiki_page.replace(i, ' ')
+
+    #Removing space characters from wiki_page list
+    wiki_page = wiki_page.split(" ")
     wiki_page_new = []
     for i in range(0, len(wiki_page)):
         if wiki_page[i] != "":
