8d769b06c74d84eb8168c3983c32d24e833af383
Paul Von Zimmerman <pcvonz@gmail.com>
2015-07-11 14:32:52 -0700

    Responsive now. Text size is based on window height, which maybe should be rethought.

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
 var n = 0;
 var data;
+var font = 0
 
 time = new Date().getTime() / 1000;
 
@@ -8,17 +9,37 @@ function draw() {
             window.cancelAnimationFrame(draw);
             getJson();
         } else {
-        curr_time = new Date().getTime() / 1000;
-        var canvas = document.getElementById('Wikipedia');
-        if(canvas.getContext){
-            var ctx = canvas.getContext('2d');
-            ctx.font = "48px serif";
-            
-            if((curr_time-time) > .2) {
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.fillText(window.variable[n], 10, 50);
-                n++;
-                time = new Date().getTime() / 1000;
+            var speed = document.getElementById('speed').value
+            curr_time = new Date().getTime() / 1000;
+            var canvas = document.getElementById('Wikipedia');
+            if(canvas.getContext){
+                //Set up canvas
+                
+                console.log($( window ).height());
+                
+                var ctx = canvas.getContext('2d');
+               
+                //get window height
+                var x = canvas.width / 2;
+                var y = canvas.height / 2;
+                
+                
+                if((curr_time-time) > (.1 / speed)) {
+                    //clear canvas and set it to size of the window
+                    canvas.width = $( window ).width();
+                    canvas.height = $( window ).height();
+                    canvas.style.width = $( window ).width().toString();
+                    canvas.style.height = $( window ).height().toString();
+                    
+                    //Calculate appropriate font size
+                    font = ($( window ).height() / 5).toString() + "px sans";
+                    console.log(font);
+                    
+                    ctx.font = font;
+                    ctx.textAlign = 'center';
+                    ctx.fillText(window.variable[n], x, y);
+                    n++;
+                    time = new Date().getTime() / 1000;
             }
             window.requestAnimationFrame(draw);
         }
@@ -36,11 +57,4 @@ function getJson() {
 )
 };
 
-function getJsonAgain() {
-    $.getJSON('http://127.0.0.1:5000/grab_article', 
-    function(data, textStatus, jqXHR) {
-        window.variable = data;
-            }
-    
-)
-};
+window.onload = getJson;
