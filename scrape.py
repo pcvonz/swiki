ffc75c746a1c794f9f73194fdeff6ab34ca889d1
Paul Von Zimmerman <pcvonz@gmail.com>
2015-07-11 14:32:19 -0700

    modified the parsing, displays complete pages without spaces

--- scrape.py
+++ scrape.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/grab_article')
 def grab_random_article():
     wiki_page = BeautifulSoup(urllib2.urlopen('http://en.wikipedia.org/wiki/Special:Random').read())
-    wiki_page = wiki_page.findAll('p')
+    wiki_page = wiki_page.findAll(id='mw-content-text')
     
     #converting bs4 result set to unicode
     wiki_page = unicode.join(u'\n',map(unicode, wiki_page))
@@ -21,18 +21,29 @@ def grab_random_article():
     tags = re.findall(r'<[^>]+>', wiki_page)
     
     #regular expression to remove all citations
-    brackets = re.findall(r'\[\d\]', wiki_page)
+    brackets = re.findall(r'\[.\]', wiki_page)
+    print(brackets)
+    returns = re.findall(r'\n', wiki_page)
     
+    tabs = re.findall(r'\t', wiki_page)
     
     for i in tags:
         wiki_page = wiki_page.replace(i, '')    
     for i in brackets:
-        print(i)
         wiki_page = wiki_page.replace(i, '')
-        
+    for i in returns:
+        wiki_page = wiki_page.replace(i, ' ')
+    #removing multiple spaces
+
+                        
     wiki_page = wiki_page.split(" ")
     
-    wiki_json = json.JSONEncoder(ensure_ascii = False).encode(wiki_page)
+    wiki_page_new = []
+    for i in range(0, len(wiki_page)):
+        if wiki_page[i] != "":
+            wiki_page_new.append(wiki_page[i])
+            
+    wiki_json = json.JSONEncoder(ensure_ascii = False).encode(wiki_page_new)
     
     #f = codecs.open('wiki_page', 'w', "utf-8")
     #f.write(wiki_json)
@@ -46,5 +57,5 @@ def main_page(name=None):
     return render_template('index.html', name=name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
